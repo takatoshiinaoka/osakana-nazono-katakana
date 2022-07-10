8d9de51e9461de9b7b66b0a70e4e934579972e32
yukinissie <bigtree2490@outlook.jp>
2022-07-10 11:29:48 +0900

    Format Function

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,23 @@ import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-const fetchMessage = async (name: String, setMessage: React.Dispatch<React.SetStateAction<string>>) => {
+const fetchMessage = async (
+  name: String,
+  setMessage: React.Dispatch<React.SetStateAction<string>>
+) => {
   const res = await fetch(`/api/SampleFunction?name=${name}`)
   const message = await res.text()
   setMessage(message)
 }
 
 function App() {
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
-    fetchMessage("inaoka", setMessage)
+    fetchMessage('inaoka', setMessage)
   }, [])
 
-  return (
-    <div>
-      {message}
-    </div>
-  )
+  return <div>{message}</div>
 }
 
 export default App
