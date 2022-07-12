fa54ef2799bc7b078871abf4662ac303247a6530
yukinissie <bigtree2490@outlook.jp>
2022-07-12 20:50:45 +0900

    yarn lintfix

--- api/FishesRead/index.ts
+++ api/FishesRead/index.ts
@@ -2,6 +2,7 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   req: HttpRequest
 ): Promise<void> {
   context.res.json(context.bindings.documents)
--- src/contexts/FishContext.tsx
+++ src/contexts/FishContext.tsx
@@ -1,11 +1,10 @@
-import {
-  createContext,
-  useState,
-  useEffect,
-} from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { fetchFishes } from '../apis/fetchFishes'
 import { Fish } from '../types/Fish'
-import { FishesContextProps, FishesContextProviderProps } from '../types/FishesContext'
+import {
+  FishesContextProps,
+  FishesContextProviderProps,
+} from '../types/FishesContext'
 
 export const FishesContext = createContext<FishesContextProps>({
   fishes: [],
@@ -13,7 +12,7 @@ export const FishesContext = createContext<FishesContextProps>({
 })
 
 export const FishesProvider = ({ children }: FishesContextProviderProps) => {
-  const [fishes, setFishes] = useState(Array<Fish>)
+  const [fishes, setFishes] = useState(Array<Fish>())
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
