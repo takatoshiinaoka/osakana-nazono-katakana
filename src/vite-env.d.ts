8fb7f028d71964aec7a28bd1d20ab298cc99c313
yukinissie <bigtree2490@outlook.jp>
2022-07-17 17:39:33 +0900

    環境変数の型を適切な場所に再定義

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_HOST: string
+  readonly VITE_DEBUG: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
