4bd47a1f340eaf85473b1ddb960eb73ef201dbe6
yukinissie <bigtree2490@outlook.jp>
2022-07-18 10:35:58 +0900

    お気持ちTopスタイリング

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,19 +1,33 @@
+import { Box, Button, HStack, VStack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 export const Top = () => {
   return (
     <div>
-      <div>御魚 なぞの カタカナ</div>
-      <Link
-        to={{
-          pathname: `/`,
-        }}
-        state={{
-          getby: 'start',
-        }}
-      >
-        Start
-      </Link>
+      <VStack justify="center" height="100vh">
+        <Box
+          bg="white"
+          height="300px"
+          width="90%"
+          css={{ borderRadius: '10px' }}
+        >
+          <VStack justify="center" height="100%" spacing={20}>
+            <div>御魚 なぞの カタカナ</div>
+            <Button size="lg">
+              <Link
+                to={{
+                  pathname: `/`,
+                }}
+                state={{
+                  getby: 'start',
+                }}
+              >
+                Start
+              </Link>
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
       {import.meta.env.VITE_DEBUG && (
         <div>
           <Link
